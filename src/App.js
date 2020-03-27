9ffbf4c6b2e28a068cf47e12e0e67e20ea27c956
Manjusha <manjushaawasthi@gmail.com>
2020-03-26 23:16:37 -0400

    Set up tools and dependencies

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+
 /* const Pet = (props) => {
     return React.createElement(
         "div", 
@@ -23,41 +26,29 @@
 } */
 
 // Destructuring props
-const Pet = ({name, animal, breed}) => {
-    return React.createElement(
-        "div", 
-        {}, 
-        [
-            React.createElement(
-                "h1",
-                {},
-                name
-            ),
-            React.createElement(
-                "h2",
-                {},
-                animal
-            ),
-            React.createElement(
-                "h2",
-                {},
-                breed
-            )
-        ]
-    )
-}
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
 
 const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {style: {margin: '5rem'}}, "Adopt Me!!"),
-            React.createElement(Pet, {name: 'Luna', animal: 'Dog', breed: 'Havanese'}),
-            React.createElement(Pet, {name: 'Pepper', animal: 'Bird', breed: 'Cockatiel'}),
-            React.createElement(Pet, {name: 'Doink', animal: 'Cat', breed: 'Mixed'}),
-        ]
-    )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", { style: { margin: "5rem" } }, "Adopt Me!!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese"
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel"
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+render(React.createElement(App), document.getElementById("root"));
