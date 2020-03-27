16c1b77576f76f63d6bb38757ae43bacc81c3818
Manjusha <manjushaawasthi@gmail.com>
2020-03-26 23:29:30 -0400

    Extract Pet component from App

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-
-/* const Pet = (props) => {
-    return React.createElement(
-        "div", 
-        {}, 
-        [
-            React.createElement(
-                "h1",
-                {},
-                props.name
-            ),
-            React.createElement(
-                "h2",
-                {},
-                props.animal
-            ),
-            React.createElement(
-                "h2",
-                {},
-                props.breed
-            )
-        ]
-    )
-} */
-
-// Destructuring props
-const Pet = ({ name, animal, breed }) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
