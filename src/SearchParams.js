9900b7de8c3eed534ab0e60f3d761bcf4393c883
Manjusha <manjushaawasthi@gmail.com>
2020-04-01 23:11:46 -0400

    Hooks: Calling the PET Api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   // Prop destructuring -> useState returns 2 items - first, current state and second, the function for updating this state
   // Best practice when using hooks -> Never define hooks under conditional(if, for) statements
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
 
   return (
     <div className="search-params">
@@ -17,6 +19,22 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value)}
+            onBlur={(e) => setAnimal(e.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map((x) => (
+              <option value={x}>{x}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
     </div>
