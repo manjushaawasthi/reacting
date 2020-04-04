b8f0eff52eeea29c04292cd094926ab48ca18062
Manjusha <manjushaawasthi@gmail.com>
2020-04-04 19:15:23 -0400

    Effects: Declaring effect and its dependencies

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown.js";
 
 const SearchParams = () => {
@@ -8,7 +8,20 @@ const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // useEffect runs after the render executes
+  useEffect(() => {
+    // pet.breeds("dog").then(console.log, console.error);
+
+    setBreeds([]);
+    setBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedNames = breeds.map(({ name }) => name);
+      console.log(breedNames);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // Define dependencies, run this effect only if one of these dependencies changes.
 
   return (
     <div className="search-params">
